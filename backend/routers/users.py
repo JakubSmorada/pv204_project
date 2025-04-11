666abf40f7bf6864d70540a30c1de3d8468322ae
smatand <andrejsmatana0@gmail.com>
2025-04-11 19:50:23 +0200

    naive protection against replay attacks

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,5 +1,7 @@
 import secrets
 import bcrypt
+import time
+from datetime import datetime, timedelta
 
 from fastapi import APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -13,19 +15,21 @@ router = APIRouter(
     tags=["users"],
 )
 
-# Proof of Work difficulty level, e. g. how many leading zeros are required in the hash
+# Proof of Work difficulty level, e.g. how many leading zeros are required in the hash
 POW_DIFFICULTY = 4
-
+CHALLENGE_TIMEOUT = 360  # 6 mins for pow
 
 @router.get("/challenge")
 async def get_challenge():
     random_string = secrets.token_hex(16)
     token = secrets.token_hex(16)
     difficulty = POW_DIFFICULTY
+    timestamp = datetime.utcnow() + timedelta(seconds=CHALLENGE_TIMEOUT)
 
     await mongodb.db.challenges.insert_one({
         "token": token,
         "challenge": random_string,
+        "timeout_at": timestamp
     })
 
     return Challenge(
@@ -43,6 +47,13 @@ async def create_user(user: User, token: str):
             raise HTTPException(status_code=400, detail="Invalid token")
 
         challenge = challenge_doc["challenge"]
+        created_at = challenge_doc["timeout_at"]
+        current_time = datetime.utcnow()
+        
+        # todo: periodic cleanup could be useful
+        if current_time > created_at:
+            await mongodb.db.challenges.delete_one({"token": token})
+            raise HTTPException(status_code=400, detail="Challenge has expired. Request a new challenge.")
 
         existing_user = await mongodb.db.users.find_one({"$or": [
             {"username": user.username}
@@ -56,8 +67,9 @@ async def create_user(user: User, token: str):
         if not ProofOfWork.verify_proof(user_dict, POW_DIFFICULTY, challenge):
             raise HTTPException(status_code=400, detail="Invalid proof of work. Got: " + str(user_dict))
 
-        # removal of unnecessary data
         await mongodb.db.challenges.delete_one({"token": token})
+
+        # removal of unnecessary data
         if "nonce" in user_dict:
             del user_dict["nonce"]
         if "hash" in user_dict:
@@ -69,7 +81,7 @@ async def create_user(user: User, token: str):
 
         result = await mongodb.db.users.insert_one(user_dict)
 
-        # insertion succcessful or not
+        # insertion successful or not
         if result.inserted_id:
             return user
         else:
